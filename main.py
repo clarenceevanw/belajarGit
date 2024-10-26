4167934d6efbe0477f2b0f88cb08a878ebbdaf5f
clarenceevanw <clarenceevan0907@gmail.com>
2024-10-26 15:02:28 +0700

    Fix: Mengubah code main

--- main.py
+++ main.py
@@ -4,13 +4,7 @@ time = 90
 while(bahrainGoal != indonesiaGoal):
     time+=1
     print(time)
-    print("Bahrain: ", bahrainGoal)
-    print("Indonesia: ", indonesiaGoal)
     if(time == 99):
         bahrainGoal +=1
-        print("Draw")
-        print("Bahrain: ", bahrainGoal)
-        print("Indonesia: ", indonesiaGoal)
-        break
-
-
+    print("Bahrain: ", bahrainGoal)
+    print("Indonesia: ", indonesiaGoal)
